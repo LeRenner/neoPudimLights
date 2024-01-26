2720a62dd96b724994cfc61dfa194648266c93b9
J. Renner <j@lerenner.dev>
2024-01-26 03:00:00 -0300

    Change to home assistant api, instead of esphome
    Signed-off-by: J. Renner <j@lerenner.dev>

--- server/main.js
+++ server/main.js
@@ -1,40 +1,60 @@
-
 const express = require('express')
 const axios = require('axios');
+const fs = require('fs');
+
 const app = express()
 const port = 3000
 
+// get token from /secret/token.txt
+const token = fs.readFileSync('/secret/token.txt', 'utf8');
+
 app.get('/1/on', (req, res) => {
-    console.log("/1/on");
+	console.log("/1/on");
 
-	const url = 'http://192.168.1.90/switch/relay_1/turn_on';
-
-	axios.post(url)
+	const url = 'https://homeassistant.pudim.xyz/api/services/switch/turn_on';
+	const headers = {
+		'Authorization': 'Bearer ' + token,
+		'Content-Type': 'application/json'
+	};
+	
+	const data = {
+		entity_id: 'switch.relay_1'
+	};
+	
+	axios.post(url, data, { headers })
 		.then(response => {
-			console.log(response.data);
+		console.log('Success:', response.data);
 		})
 		.catch(error => {
-			console.log(error);
+		console.error('Error:', error.response ? error.response.data : error.message);
 		});
 
 	res.send('Sucess!');
 });
 
 app.get('/1/off', (req, res) => {
-	console.log("/1/off");
+	console.log("/1/on");
 
-	const url = 'http://192.168.1.90/switch/relay_1/turn_off';	
-
-	axios.post(url)
+	const url = 'https://homeassistant.pudim.xyz/api/services/switch/turn_off';
+	const headers = {
+		'Authorization': 'Bearer ' + token,
+		'Content-Type': 'application/json'
+	};
+	
+	const data = {
+		entity_id: 'switch.relay_1'
+	};
+	
+	axios.post(url, data, { headers })
 		.then(response => {
-			console.log(response.data);
+		console.log('Success:', response.data);
 		})
 		.catch(error => {
-			console.log(error);
+		console.error('Error:', error.response ? error.response.data : error.message);
 		});
-
+	
 	res.send('Sucess!');
-})
+});
 
 app.listen(port, () => {
 	console.log(`Application listening on port ${port}`)
