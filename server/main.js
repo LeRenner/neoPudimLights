67bdd2b2fc5d094196e1123e31de016c505f1595
J. Renner <j@lerenner.dev>
2024-03-31 21:38:41 -0300

    Add abajour 2
    Signed-off-by: J. Renner <j@lerenner.dev>

--- server/main.js
+++ server/main.js
@@ -59,6 +59,54 @@ app.get('/1/off', (req, res) => {
 	res.send('Sucess!');
 });
 
+app.get('/2/on', (req, res) => {
+	console.log("/2/on");
+
+	const url = 'https://homeassistant.pudim.xyz/api/services/switch/turn_on';
+	const headers = {
+		'Authorization': 'Bearer ' + token,
+		'Content-Type': 'application/json'
+	};
+	
+	const data = {
+		entity_id: 'switch.relay_1_3'
+	};
+	
+	axios.post(url, data, { headers })
+		.then(response => {
+		console.log('Success:', response.data);
+		})
+		.catch(error => {
+		console.error('Error:', error.response ? error.response.data : error.message);
+		});
+
+	res.send('Sucess!');
+});
+
+app.get('/2/off', (req, res) => {
+	console.log("/2/on");
+
+	const url = 'https://homeassistant.pudim.xyz/api/services/switch/turn_off';
+	const headers = {
+		'Authorization': 'Bearer ' + token,
+		'Content-Type': 'application/json'
+	};
+	
+	const data = {
+		entity_id: 'switch.relay_1_3'
+	};
+	
+	axios.post(url, data, { headers })
+		.then(response => {
+		console.log('Success:', response.data);
+		})
+		.catch(error => {
+		console.error('Error:', error.response ? error.response.data : error.message);
+		});
+	
+	res.send('Sucess!');
+});
+
 app.listen(port, () => {
 	console.log(`Application listening on port ${port}`)
 })
